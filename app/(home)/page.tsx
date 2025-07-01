ed5c2465d6082d7242a4b4221a26916b00bfd415
Meitekini Leakey <meitekinileakey@gmail.com>
2025-07-01 10:35:06 +0100

    Created home carousel

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,6 @@
-import { Button } from "@/components/ui/button";
-
+import { HomeCarousel } from "@/components/shared/home/home-carousel";
+import data from "@/lib/data";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    <Button>Hello World</Button>
-    </div>
-  );
+  return <HomeCarousel items={data.carousels} />;
 }
